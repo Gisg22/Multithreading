28d5d0e1d1fce680f613756e7696f76c0e2c3081
Gisg <75253734+Gisg22@users.noreply.github.com>
2021-07-18 01:57:58 +0600

    Add files via upload

---
+++ treadMultithreading.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+void DoWork() {
+    for (size_t i = 0; i < 10; i++)
+    {
+        cout << "ID: " << this_thread::get_id() << "\tDoWork\t"<<i << endl;
+        this_thread::sleep_for(chrono::milliseconds(1000));
+    }
+}
+
+int main()
+{
+   /* cout << "START MAIN" << endl;
+    this_thread::sleep_for(chrono::milliseconds(500));
+    cout << this_thread::get_id() << endl;;
+    cout << "END MAIN" << endl; */
+
+    thread th(DoWork);
+    
+    for (size_t i = 0; i < 10; i++)
+    {
+        cout << "ID: " << this_thread::get_id() <<"\tmain\t"<< i <<endl;
+        this_thread::sleep_for(chrono::milliseconds(500));
+    }
+    th.join();
+    return 0;
+    
+}
+
