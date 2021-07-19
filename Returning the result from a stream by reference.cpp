88843a3707a1dfa554839cdda7cf521574de2a8b
Gisg <75253734+Gisg22@users.noreply.github.com>
2021-07-19 11:39:48 +0600

    Add files via upload

---
+++ Returning the result from a stream by reference.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+void DoWork(int &a) {
+
+	this_thread::sleep_for(chrono::milliseconds(5000));
+
+    cout << "ID: " << this_thread::get_id() << "============\t" << "DoWork STARTED\t===============" << endl;
+    
+	this_thread::sleep_for(chrono::milliseconds(5000));
+
+    a *= 2;
+
+    cout << "ID: " << this_thread::get_id()  << "============\t" << "DoWork END\t===============" << endl;
+
+    
+
+}
+
+int main()
+{
+int q = 5;
+
+thread th(DoWork,std::ref( q));
+
+
+
+    for (size_t i = 0; i < 10; i++)
+    {
+        cout << "ID: " << this_thread::get_id() << "\tmain\t" << i << endl;
+        this_thread::sleep_for(chrono::milliseconds(500));
+    } 
+
+   th.join();
+   cout << q << endl;
+    return 0;
+}
