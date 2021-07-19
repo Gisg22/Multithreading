31f6c122aa3a2b1daab6522057383600ed7da2f0
Gisg <75253734+Gisg22@users.noreply.github.com>
2021-07-19 14:04:11 +0600

    Add files via upload

---
+++ lambdaMultithreading.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+int Sum(int a, int b) {
+
+    this_thread::sleep_for(chrono::milliseconds(5000));
+
+    cout << "ID: " << this_thread::get_id() << "============\t" << "DoWork STARTED\t===============" << endl;
+
+    this_thread::sleep_for(chrono::milliseconds(5000));
+
+
+    cout << "ID: " << this_thread::get_id() << "============\t" << "DoWork END\t===============" << endl;
+
+    
+    return a + b;
+}
+
+int main()
+{
+    int q = 5;
+    int y = 4;
+
+    int result;
+    thread th([&result, q, y]() {
+        result = Sum(q, y);
+        });
+   
+   
+    for (size_t i = 0; i < 10; i++)
+    {
+        cout << "ID: " << this_thread::get_id() << "\tmain\t" << i << endl;
+        this_thread::sleep_for(chrono::milliseconds(500));
+    } 
+
+    th.join();
+    cout << result << endl;
+    return 0;
+}
