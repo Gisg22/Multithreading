62b62653773cbd5faee9e671feefaf73bc188644
Gisg <75253734+Gisg22@users.noreply.github.com>
2021-07-20 22:42:14 +0600

    Add files via upload

---
+++ unique_lock mutex.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <mutex>
+#include <thread>
+
+using namespace std;
+
+mutex _mtx;
+
+void Print(char ch) {
+	this_thread::sleep_for(chrono::milliseconds(2000));
+	unique_lock<mutex> ul(_mtx, defer_lock);
+
+
+	for (int i = 0; i < 5; i++)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			cout << ch;
+			this_thread::sleep_for(chrono::milliseconds(10));
+		}
+		cout << endl;
+	}
+	cout << endl;
+	ul.unlock();
+	this_thread::sleep_for(chrono::milliseconds(2000));
+	
+}
+
+int main()
+{
+   // thread th1(Foo, 10);
+    //thread th2(Foo, 10);
+	thread th1(Print, '*');
+	thread th2(Print, '#');
+   th1.join();
+ th2.join();
+
+    return 0;
+}
+
